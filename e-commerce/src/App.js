e03850b5dda5f819de5abcedd38c3d8e3a4bfbac
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-02-10 02:52:44 -0500

    routes new version

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-
-import Home from './pages/homepage/Home';
+import Home from "./pages/homepage/Home";
 
 function App() {
   return (
-    <div>
-    <Home/>
-    </div>
+    <>
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      </Routes>
+    </>
   );
 }
 
