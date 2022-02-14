54bbeb4cee92e0118c3a8af724ecff1342fe0f99
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-02-14 12:32:30 -0500

    slider

--- e-commerce/src/components/banner/Slider.jsx
+++ e-commerce/src/components/banner/Slider.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
-
-import {IoIosArrowDroprightCircle,IoIosArrowDropleftCircle} from 'react-icons/io';
-
-import {MdOutlineArrowForwardIos} from "react-icons/md"
+import {
+  IoIosArrowDroprightCircle,
+  IoIosArrowDropleftCircle,
+} from "react-icons/io";
 
 import sliderStyle from "./sliderStyle.scss";
 
@@ -13,26 +12,60 @@ import { sliderItems } from "../../directory.data";
 const Slider = () => {
   const [slideImg, setSlideImg] = useState(0);
 
+  const autoPlay = () => {
+    setTimeout(() => {
+      
+        setSlideImg(slideImg === sliderItems.length - 1 ? 0 : slideImg + 1);
+    
+    }, 3000);
+  };
+
+  
+  useEffect(()=>{
+      autoPlay()
+      return(
+          clearTimeout()
+      )
+  },[slideImg])
+
+  const nextSlide = () => {
+    setSlideImg(slideImg === sliderItems.length - 1 ? 0 : slideImg + 1);
+  };
+
+  const previousSlide = () => {
+
+    setSlideImg(slideImg === 0 ? sliderItems.length - 1 : slideImg - 1);
+  };
+
   return (
-    <div className="container">
-      <div className="arrowRight">
-        <IoIosArrowDroprightCircle color="white" fontSize="40px"/>
+    <>
+      <div className="arrowRight" direction="right" onClick={nextSlide}>
+        <IoIosArrowDroprightCircle className="iconRight" fontSize="60px" />
       </div>
 
-      <div className="wrapper">
-        {sliderItems.map(({ imageUrl, id }) => {
-          return (
-            <div key={id} className="wrapper">
-              <img src={imageUrl} alt="" className="bannerImg" />
-            </div>
-          );
-        })}
-      </div>
+      {sliderItems.map(({ imageUrl, id }, index) => {
+        return (
+          <div className={index === slideImg ? "activeSlide" : "slide"}>
+            {index === slideImg && (
+              <img
+                key={id}
+                src={imageUrl}
+                alt="fashion image"
+                className="bannerImg"
+              ></img>
+            )}
+          </div>
+        );
+      })}
 
-      <div className="arrowLeft">
-        <IoIosArrowDropleftCircle color="white" fontSize="40px"/>
+      <div className="arrowLeft" direction="left">
+        <IoIosArrowDropleftCircle
+          fontSize="60px"
+          className="iconLeft"
+          onClick={previousSlide}
+        />
       </div>
-    </div>
+    </>
   );
 };
 
--- e-commerce/src/components/directory/Directory.jsx
+++ e-commerce/src/components/directory/Directory.jsx
@@ -3,15 +3,15 @@ import React, { useState } from "react";
 import sections from "../../directory.data";
 import MenuItem from "../menuItem/MenuItem";
 
-import directoryStyle from "./directoryStyle.scss"
+import directoryStyle from "./directoryStyle.scss";
 
 const Directory = () => {
-
-
   return (
     <div className="directoryMenu">
       {sections.map(({ title, imageUrl, id, size }) => {
-       return( <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>);
+        return (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        );
       })}
     </div>
   );
