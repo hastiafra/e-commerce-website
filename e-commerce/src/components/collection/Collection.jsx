84f27de09641504f563678f42983bae95c14d706
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-02-21 14:17:52 -0500

    styling collection

--- e-commerce/src/components/collection/Collection.jsx
+++ e-commerce/src/components/collection/Collection.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-
+import collectionStyle from "./collectionStyle.scss"
 const Collection = ({ name, imageUrl, price }) => {
 
   return (
-    <>
+    <div className="wrapperCollection">
       <h1>{name}</h1>
-      <img src={imageUrl}></img>
+      <img className="img" src={imageUrl}></img>
       <h4>Price: {price}</h4>
-    </>
+    </div>
   );
 };
 
--- e-commerce/src/pages/shopPage/Shop.jsx
+++ e-commerce/src/pages/shopPage/Shop.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Collection from "../../components/collection/Collection";
+import shopStyle from "./shopStyle.scss"
 
 import SHOP_DATA from "../../data/shop.data";
 
@@ -16,15 +17,21 @@ const Shop = () => {
   let title = clickedTitle[0].title;
 
   return (
-    <>
-    <h1>{title}</h1>
-
-      {itemArr.map(({ id, name, imageUrl, price }) => {
-        return (
-          <Collection key={id} name={name} imageUrl={imageUrl} price={price} />
-        );
-      })}
-    </>
+    <div className="wrapperShop">
+      <h1 className="title">{title}</h1>
+      <div className="containerShop">
+        {itemArr.map(({ id, name, imageUrl, price }) => {
+          return (
+            <Collection
+              key={id}
+              name={name}
+              imageUrl={imageUrl}
+              price={price}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
