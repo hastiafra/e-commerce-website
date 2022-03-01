28c5b3bd40a1ee511b2c6981a046e6a3982159b8
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-03-01 18:49:30 -0500

    setting up sign in form

--- e-commerce/src/components/signIn/SignIn.jsx
+++ e-commerce/src/components/signIn/SignIn.jsx
@@ -6,7 +6,15 @@ const SignIn = () => {
 
   const signInSubmit = (e) => {
     e.preventDefault();
+    setLoginInfo({ email: "", password: "" });
   };
+
+  const getInfo = (e) => {
+    const { name, value } = e.target;
+    setLoginInfo(prevState => ({...prevState, [name]: value}))
+  };
+
+  
 
   return (
     <div className="signInContainer">
@@ -15,9 +23,21 @@ const SignIn = () => {
 
       <form onSubmit={signInSubmit} className="signInForm">
         <label htmlFor="email">Email</label>
-        <input type="email" required />
+        <input
+          type="email"
+          value={loginInfo.email || ""}
+          name="email"
+          onChange={getInfo}
+          required
+        />
         <label htmlFor="password">Password</label>
-        <input type="current-password" required />
+        <input
+          type="password"
+          value={loginInfo.password || ""}
+          name="password"
+          onChange={getInfo}
+          required
+        />
         <button type="submit">Login</button>
       </form>
     </div>
