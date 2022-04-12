cdb862b2b67c565158fac37bc0dc7a465241b3fb
hastiafra <77177165+hastiafra@users.noreply.github.com>
2022-04-12 09:38:47 -0400

    setting up usecontext

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,25 +1,22 @@
 import "./App.css";
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 
 import Home from "./pages/homepage/Home";
-import Shop from "./pages/shopPage/Shop";
 import Header from "./components/header/Header";
 import SignInUp from "./pages/signInPage/SignInUp";
-
+import Shop from "./pages/shopPage/Shop";
 
 function App() {
-  return (<>
-  <Header/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop/:titleId" element={<Shop />} />
-          <Route path="/auth" element={<SignInUp />} />
-
-
-
-        </Routes>
-        </>
-  
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop/:titleId" element={<Shop />} />
+        <Route path="/auth" element={<SignInUp />} />
+      </Routes>
+    </>
   );
 }
 
--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -1,13 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-
 import "./index.css";
 import App from "./App";
+import { UserProvider } from "./contexts/user.context";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
